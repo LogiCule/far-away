2dccb26d606879473854a63954475b2b4496daa0
Rahul Dubey <rahuldubey7398@gmail.com>
2024-08-10 09:49:49 +0530

    style

--- src/components/Stats/index.jsx
+++ src/components/Stats/index.jsx
@@ -2,7 +2,7 @@ const Stats = ({ items }) => {
   const total = items.length;
   const done = items.filter((item) => item.packed).length;
   return (
-    <footer>
+    <footer className="stats">
       <em>
         You have {total} items on your list, and you already packed {done} (
         {Math.round((done * 100) / total)}%)
